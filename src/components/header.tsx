46346d76fd5445c565e98dfd94ffaee50da02e03
zecamilplano <ezequiasgama2@gmail.com>
2026-02-23 15:47:52 -0300

    feat(header): adiciona layout do header

---
+++ src/components/header.tsx
@@ -0,0 +1,12 @@
+import { Logo } from "@/components/icons/logo"
+
+function Header() {
+  return (
+    <header className=" bg-green-800 flex justify-center items-center ">
+      <Logo />
+    </header >
+
+  )
+}
+
+export default Header
