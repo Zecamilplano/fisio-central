917a84cf7d4c9e4022099d0c8a71fb25b7f42104
zecamilplano <ezequiasgama2@gmail.com>
2026-07-15 15:05:44 -0300

    fix(sub header form register): concerto de erro de não exibir o titulo se não enviar o parâmentro IMG

--- src/components/patient/register/SubHeaderFormRegister.tsx
+++ src/components/patient/register/SubHeaderFormRegister.tsx
@@ -8,9 +8,6 @@ function SubHeaderFormRegister({
   subtitle,
   icon,
 }: SubHeaderFormRegisterType) {
-  if (!img) return null
-  const { dragEvents, isDragging, image, removeImage, handleFileChange } = img
-
   return (
     <fieldset className="font-montserrat my-8 flex flex-col items-center">
       <legend className="text-2xl md:text-3xl text-center text-[#2D2D2D] font-semibold tracking-wide">
@@ -23,10 +20,11 @@ function SubHeaderFormRegister({
       {/*Adicionar foto de perfil*/}
       <div className="flex flex-col items-center">
         {icon === "person" &&
-          (image.length === 0 ? (
+          img &&
+          (img.image.length === 0 ? (
             <>
               <label
-                {...dragEvents}
+                {...img.dragEvents}
                 htmlFor="photo"
                 className="group bg-[#FFE0B2] hover:bg-[#FFCC80] active:bg-[#FFB74D] w-22.5 h-22.5 rounded-[50%] flex justify-center items-center cursor-pointer mt-6"
               >
@@ -41,15 +39,15 @@ function SubHeaderFormRegister({
                 id="photo"
                 name="photo"
                 accept="image/*"
-                onChange={(e) => handleFileChange(e)}
+                onChange={(e) => img.handleFileChange(e)}
                 hidden
               />
             </>
           ) : (
-            image.map((image, index) => (
+            img.image.map((image, index) => (
               <div key={index} className="flex flex-col items-center mt-6">
                 <div
-                  className={`flex relative w-22.5 h-22.5 group ${isDragging ? "opacity-50" : "opacity-100"}`}
+                  className={`flex relative w-22.5 h-22.5 group ${img.isDragging ? "opacity-50" : "opacity-100"}`}
                   key={image.name}
                 >
                   <Image
@@ -64,7 +62,7 @@ function SubHeaderFormRegister({
                   type="button"
                   onClick={(e) => {
                     e.stopPropagation()
-                    removeImage()
+                    img.removeImage()
                   }}
                   className="flex gap-2 bg-[#FBBF24] text-[#78350F] hover:opacity-80 active:opacity-60 px-3 py-3 w-full h-full mt-3 rounded-lg"
                 >
